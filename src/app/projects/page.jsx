7cecb899432c79b2ae8bd8bfa6b51c4456599f1f
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-10-08 23:01:00 +0500

    .

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -21,14 +21,6 @@ const Projects = () => {
             <main>
                 <div className="container">
                     <section className="portfolio">
-                        
-                        {/* <div className="portfolio-header">
-                            <h2>Selected works</h2>
-                            <a href="/projects" className="btn">
-                                View All Works
-                                <img src="/img/icons/arrow-right-dark.svg" alt="" />
-                            </a>
-                        </div> */}
 
                         <div className="portfolio-item">
                             <img className="project-preview" src="/img/portfolio/project-1.png" alt="" />
@@ -60,10 +52,10 @@ const Projects = () => {
                             <img className="project-preview" src="/img/portfolio/project-3.png" alt="" />
                             <div className="portfolio-item__text">
                                 <div>
-                                    <strong>Kivi Studio</strong>
+                                    <strong>Bestune Motors</strong>
                                     <p>Website</p>
                                 </div>
-                                <a href="https://kivistudio.uz/" target="_blank" className="visit-btn">
+                                <a href="https://bestuneuzbekistan.uz/" target="_blank" className="visit-btn">
                                     <img src="/img/icons/arrow-right-dark.svg" alt="" />
                                 </a>
                             </div>
@@ -73,10 +65,75 @@ const Projects = () => {
                             <img className="project-preview" src="/img/portfolio/project-4.png" alt="" />
                             <div className="portfolio-item__text">
                                 <div>
+                                    <strong>Dongfeng Motors</strong>
+                                    <p>E-commerse / Web Site</p>
+                                </div>
+                                <a href="https://dongfenguzbekistan.uz/" target="_blank" className="visit-btn">
+                                    <img src="/img/icons/arrow-right-dark.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="portfolio-item">
+                            <img className="project-preview" src="/img/portfolio/project-5.png" alt="" />
+                            <div className="portfolio-item__text">
+                                <div>
+                                    <strong>Kivi Studio</strong>
+                                    <p>E-commerse / Web Site</p>
+                                </div>
+                                <a href="https://kivistudio.uz/" target="_blank" className="visit-btn">
+                                    <img src="/img/icons/arrow-right-dark.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="portfolio-item">
+                            <img className="project-preview" src="/img/portfolio/project-6.png" alt="" />
+                            <div className="portfolio-item__text">
+                                <div>
                                     <strong>Mold Components</strong>
                                     <p>E-commerse / Web App</p>
                                 </div>
                                 <a href="https://mold-react-september.vercel.app/" target="_blank" className="visit-btn">
+                                    <img src="/img/icons/arrow-right-dark.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="portfolio-item">
+                            <img className="project-preview" src="/img/portfolio/project-7.png" alt="" />
+                            <div className="portfolio-item__text">
+                                <div>
+                                    <strong>Downloadlify</strong>
+                                    <p>Software / Web App / Startup</p>
+                                </div>
+                                <a href="https://downloadlify.vercel.app/" target="_blank" className="visit-btn">
+                                    <img src="/img/icons/arrow-right-dark.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="portfolio-item">
+                            <img className="project-preview" src="/img/portfolio/project-8.png" alt="" />
+                            <div className="portfolio-item__text">
+                                <div>
+                                    <strong>TS Team</strong>
+                                    <p>E-commerse / Web Site</p>
+                                </div>
+                                <a href="https://tsteam.aero/" target="_blank" className="visit-btn">
+                                    <img src="/img/icons/arrow-right-dark.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="portfolio-item">
+                            <img className="project-preview" src="/img/portfolio/project-9.png" alt="" />
+                            <div className="portfolio-item__text">
+                                <div>
+                                    <strong>Eco Ice</strong>
+                                    <p>E-commerse / Web Site</p>
+                                </div>
+                                <a href="https://eco-ice.uz/" target="_blank" className="visit-btn">
                                     <img src="/img/icons/arrow-right-dark.svg" alt="" />
                                 </a>
                             </div>
